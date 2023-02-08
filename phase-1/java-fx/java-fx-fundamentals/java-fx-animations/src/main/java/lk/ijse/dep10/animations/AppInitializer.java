00190c8c46a7fd318c2aa24ae854ca3421bd4a29
VipulaNeranjana <vipulana17@gmail.com>
2023-02-08 10:03:02 +0530

    corrected java animation project

--- phase-1/java-fx/java-fx-fundamentals/java-fx-animations/src/main/java/lk/ijse/dep10/animations/AppInitializer.java
+++ phase-1/java-fx/java-fx-fundamentals/java-fx-animations/src/main/java/lk/ijse/dep10/animations/AppInitializer.java
@@ -19,8 +19,8 @@ import java.time.format.DateTimeFormatter;
 
 public class AppInitializer extends Application {
 
-    private  Stage stageTransition;
-    private  Stage stageAnimation;
+    private Stage stageTransition;
+    private Stage stageAnimation;
 
     public static void main(String[] args) {
         launch(args);
@@ -28,24 +28,21 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.show();
         mainScene(primaryStage);
-
+        primaryStage.show();
     }
 
-    public void mainScene(Stage stage){
-        Button transitionButton = new Button("transition");
-        Button animationButton = new Button("animation");
-        Label label = new Label("Java fx animation model");
-        Label lblTime = new Label(LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss")).toString());
-        VBox vBox = new VBox(label,animationButton,transitionButton,lblTime);
-        Scene scene = new Scene(vBox);
+    private void mainScene(Stage stage){
+        Label lblTitle = new Label("Java FX Animation Module");
+        Button btnTransitions = new Button("Transitions");
+        Button btnAnimations = new Button("Animations");
+        Label lblTime = new Label(LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss")));
 
-        KeyFrame keyFrame = new KeyFrame(Duration.seconds(1), actionEvent -> {
-            lblTime.setText(LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss")).toString());
+        KeyFrame key1 = new KeyFrame(Duration.seconds(1), event -> {
+            lblTime.setText(LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss")));
         });
 
-        Timeline timeline = new Timeline(keyFrame);
+        Timeline timeline = new Timeline(key1);
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.playFromStart();
 
@@ -53,151 +50,206 @@ public class AppInitializer extends Application {
         lblTime.setMaxWidth(Double.MAX_VALUE);
         lblTime.setAlignment(Pos.CENTER);
 
-        vBox.setMinHeight(800);
-        vBox.setMinWidth(600);
-        vBox.setPadding(new Insets(30));
-        label.setAlignment(Pos.CENTER);
-        label.setFont(new Font(30));
-        label.setMaxWidth(Double.MAX_VALUE);
-        animationButton.setMaxWidth(Double.MAX_VALUE);
-        transitionButton.setMaxWidth(Double.MAX_VALUE);
-        animationButton.setAlignment(Pos.CENTER);
-        transitionButton.setAlignment(Pos.CENTER);
-        vBox.setSpacing(20);
+        lblTitle.setFont(new Font(24));
+        lblTitle.setMaxWidth(Double.MAX_VALUE);
+        lblTitle.setAlignment(Pos.CENTER);
+        lblTitle.setPadding(new Insets(0,0,5,0));
 
-        animationButton.setOnAction(actionEvent -> animationScene(new Stage()));
-        transitionButton.setOnAction(actionEvent -> transitionScene(new Stage()));
+        Font font = new Font(16);
+        btnTransitions.setFont(font);
+        btnAnimations.setFont(font);
+        btnTransitions.setMaxWidth(Double.MAX_VALUE);
+        btnAnimations.setMaxWidth(Double.MAX_VALUE);
 
-        stage.setScene(scene);
-        stage.centerOnScreen();
-        stage.show();
+        btnTransitions.setOnAction(event -> transitionStage());
+        btnAnimations.setOnAction(event -> animationStage());
 
+        VBox root = new VBox(lblTitle, btnTransitions, btnAnimations, lblTime);
+        root.setSpacing(20);
+        root.setBackground(Background.fill(Color.WHITE));
+        root.setPadding(new Insets(10));
 
-
+        Scene mainScene = new Scene(root);
+        stage.setScene(mainScene);
+        stage.setTitle("Java FX Animations Demo");
+        stage.setWidth(400);
     }
 
-    private void transitionScene(Stage stage) {
-        if(stageTransition != null) return;
-        stageTransition = stage;
-        stage.setOnCloseRequest(windowEvent -> {
-            stageTransition = null;
-        });
-
-        Button btnPlay = new Button("PLAY");
-        Button btnStop = new Button("STOP");
-        Label lblPreview = new Label("Preview");
-
-        lblPreview.setFont(new Font(30));
-        VBox vBox = new VBox(btnPlay,btnStop);
-        HBox root = new HBox(vBox,lblPreview);
-        Scene scene = new Scene(root);
-
-        vBox.setSpacing(10);
-        vBox.setPadding(new Insets(0,10,0,0));
-        btnPlay.setMaxWidth(Double.MAX_VALUE);
-        btnStop.setMaxWidth(Double.MAX_VALUE);
-        lblPreview.setAlignment(Pos.CENTER);
-        lblPreview.setMaxWidth(Double.MAX_VALUE);
-        lblPreview.setMaxHeight(Double.MAX_VALUE);
-        HBox.setHgrow(lblPreview,Priority.ALWAYS);
-        lblPreview.setBackground(Background.fill(Color.LIGHTBLUE));
-        root.setPadding(new Insets(20));
-
-        KeyFrame key1 = new KeyFrame(Duration.millis(500),actionEvent -> {
-            lblPreview.setTextFill(Color.RED);
-        });
-        KeyFrame key2 = new KeyFrame(Duration.millis(750),actionEvent -> {
-            lblPreview.setScaleX(1.5);
-            lblPreview.setScaleY(1.5);
-        });
-        KeyFrame key3 = new KeyFrame(Duration.millis(1000),actionEvent -> {
-            lblPreview.setTextFill(Color.BLACK);
-
-        });
-        KeyFrame key4 = new KeyFrame(Duration.millis(1250),actionEvent -> {
-            lblPreview.setScaleX(1);
-            lblPreview.setScaleY(1);
-        });
-        KeyFrame key5 = new KeyFrame(Duration.millis(1500),actionEvent -> {
-            lblPreview.setTextFill(Color.PURPLE);
-
-        });
-
-        Timeline timeline = new Timeline(key1,key2,key3,key4,key5);
-        timeline.setCycleCount(Animation.INDEFINITE);
-
-        btnPlay.setOnAction(actionEvent -> {
-            timeline.play();
-        });
-
-        btnStop.setOnAction(actionEvent -> {
-            timeline.stop();
-        });
-
-        root.setMinWidth(900);
-        root.setMinHeight(700);
-
-        stage.setScene(scene);
-        stage.centerOnScreen();
-        stage.show();
+    private void transitionStage(){
+        if (stageTransition != null) return;
+        stageTransition = new Stage();
+        stageTransition.setOnCloseRequest(event -> stageTransition = null);
+        stageTransition.setTitle("Transitions");
+        transitionScene();
+        stageTransition.show();
+        stageTransition.centerOnScreen();
     }
 
-    private void animationScene(Stage stage) {
-        if(stageAnimation != null) return;
-        stageAnimation =stage;
-        stage.setOnCloseRequest(windowEvent -> {
-            stageAnimation = null;
-        });
-
+    private void transitionScene(){
+        Button btnReset = new Button("Reset");
         Button btnFadeIn = new Button("Fade In");
         Button btnFadeOut = new Button("Fade Out");
         Button btnScale = new Button("Scale");
         Button btnRotate = new Button("Rotate");
-        Button btnTranslateX = new Button("Translate x");
-        Button btnTranslateY = new Button("Translate y");
+        Button btnTranslateX = new Button("Translate X");
+        Button btnTranslateY = new Button("Translate Y");
 
-        HBox hBox = new HBox(btnFadeIn,btnFadeOut,btnScale,btnRotate,btnTranslateX,btnTranslateY);
-        Label lblPreview = new Label("Preview");
+        HBox hBox = new HBox(btnReset, btnFadeIn, btnFadeOut, btnScale, btnRotate, btnTranslateX, btnTranslateY);
+        Label lblPreview = new Label("PREVIEW");
 
-        VBox root = new VBox(hBox,lblPreview);
-        Scene scene = new Scene(root);
+        VBox root = new VBox(hBox, lblPreview);
 
-        root.setMinHeight(800);
-        root.setMinWidth(600);
+        root.setPrefWidth(800);
+        root.setPrefHeight(400);
+        root.setPadding(new Insets(10));
+        root.setBackground(Background.fill(Color.WHITE));
+        root.setSpacing(150);
 
         hBox.setSpacing(10);
-        Font font  = new Font(16);
+        Font font = new Font(16);
         for (Node child : hBox.getChildren()) {
-            child = (Button) child;
-            ((Button) child).setFont(font);
-            ((Button) child).setMaxWidth(Double.MAX_VALUE);
-            HBox.setHgrow(child, Priority.ALWAYS);
+            Button btn = (Button) child;
+            btn.setFont(font);
+            btn.setMaxWidth(Double.MAX_VALUE);
+            HBox.setHgrow(btn, Priority.ALWAYS);
         }
 
         lblPreview.setMaxWidth(Double.MAX_VALUE);
         lblPreview.setAlignment(Pos.CENTER);
         lblPreview.setFont(new Font(44));
-        root.setSpacing(150);
+        lblPreview.setTextFill(Color.LIGHTGRAY);
 
-        btnFadeOut.setOnAction(actionEvent -> {
-            FadeTransition fadeOut = new FadeTransition(Duration.seconds(5),lblPreview);
+        Scene scene = new Scene(root);
+        stageTransition.setScene(scene);
+        stageTransition.setMinWidth(700);
+
+        btnFadeIn.setOnAction(event -> {
+            FadeTransition fadeIn = new FadeTransition(Duration.seconds(2), lblPreview);
+            fadeIn.setFromValue(0);
+            fadeIn.setToValue(1);
+            fadeIn.play();
+        });
+
+        btnFadeOut.setOnAction(event -> {
+            FadeTransition fadeOut = new FadeTransition(Duration.seconds(2), lblPreview);
             fadeOut.setFromValue(1);
             fadeOut.setToValue(0);
             fadeOut.play();
-            fadeOut.setAutoReverse(true);
         });
 
-        btnRotate.setOnAction(actionEvent -> {
-            RotateTransition rotate = new RotateTransition(Duration.seconds(5),lblPreview);
+        btnScale.setOnAction(event -> {
+            ScaleTransition scale = new ScaleTransition(Duration.seconds(1), lblPreview);
+            scale.setFromX(1);
+            scale.setToY(1);
+            scale.setToX(2);
+            scale.setToY(2);
+            scale.play();
+        });
+
+        btnRotate.setOnAction(event -> {
+            RotateTransition rotate = new RotateTransition(Duration.seconds(1), lblPreview);
             rotate.setFromAngle(0);
             rotate.setToAngle(180);
             rotate.play();
-            rotate.setAutoReverse(true);
-
         });
 
-        stage.setScene(scene);
-        stage.centerOnScreen();
-        stage.show();
+        btnTranslateX.setOnAction(event -> {
+            TranslateTransition translateX = new TranslateTransition(Duration.seconds(1), lblPreview);
+            translateX.setFromX(-500);
+            translateX.setToX(0);
+            translateX.play();
+        });
+
+        btnTranslateY.setOnAction(event -> {
+            TranslateTransition translateY = new TranslateTransition(Duration.seconds(1), lblPreview);
+            translateY.setFromY(-500);
+            translateY.setToY(0);
+            translateY.play();
+        });
+
+        btnReset.setOnAction(event -> {
+            lblPreview.setScaleX(1);
+            lblPreview.setScaleY(1);
+            lblPreview.setRotate(0);
+            lblPreview.setOpacity(1);
+        });
+    }
+
+    private void animationStage(){
+        if (stageAnimation != null) return;
+        stageAnimation = new Stage();
+        stageAnimation.setOnCloseRequest(event -> stageAnimation = null);
+        stageAnimation.setTitle("Animations");
+        animationScene();
+        stageAnimation.show();
+        stageAnimation.centerOnScreen();
+    }
+
+    private void animationScene(){
+        Button btnPlay = new Button("Play");
+        Button btnStop = new Button("Stop");
+
+        VBox vBox = new VBox(btnPlay, btnStop);
+
+        Label lblPreview = new Label("PREVIEW");
+        StackPane stackPane = new StackPane(lblPreview);
+
+        HBox root = new HBox(vBox, stackPane);
+
+        root.setSpacing(10);
+        root.setPadding(new Insets(10));
+        root.setBackground(Background.fill(Color.WHITE));
+
+        stackPane.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(stackPane, Priority.ALWAYS);
+        stackPane.setBorder(Border.stroke(Color.LIGHTGRAY));
+
+        lblPreview.setFont(new Font(44));
+        lblPreview.setTextFill(Color.DARKGRAY);
+
+        vBox.setSpacing(10);
+
+        Font font = new Font(16);
+        btnPlay.setMinWidth(150);
+        btnStop.setMinWidth(150);
+        btnPlay.setMaxWidth(Double.MAX_VALUE);
+        btnStop.setMaxWidth(Double.MAX_VALUE);
+        btnPlay.setFont(font);
+        btnStop.setFont(font);
+
+        Scene scene = new Scene(root);
+        stageAnimation.setMinWidth(500);
+        stageAnimation.setMinHeight(130);
+        stageAnimation.setScene(scene);
+
+        KeyFrame key1 = new KeyFrame(Duration.millis(500), event-> {
+            lblPreview.setTextFill(Color.RED);
+        });
+
+        KeyFrame key2 = new KeyFrame(Duration.millis(1000), event-> {
+            lblPreview.setScaleX(1.5);
+            lblPreview.setScaleY(1.5);
+        });
+
+        KeyFrame key3 = new KeyFrame(Duration.millis(1250), event-> {
+            lblPreview.setTextFill(Color.GREEN);
+            lblPreview.setScaleX(1);
+            lblPreview.setScaleY(1);
+        });
+
+        KeyFrame key4 = new KeyFrame(Duration.millis(1800), event-> {
+            lblPreview.setTranslateX(50);
+        });
+
+        KeyFrame key5 = new KeyFrame(Duration.millis(2500), event-> {
+            lblPreview.setTextFill(Color.BLUE);
+            lblPreview.setTranslateX(0);
+        });
+
+        Timeline timeline = new Timeline(key1, key2, key3, key4, key5);
+        timeline.setCycleCount(Animation.INDEFINITE);
+
+        btnPlay.setOnAction(event -> timeline.playFromStart());
+        btnStop.setOnAction(event -> timeline.stop());
     }
 }
