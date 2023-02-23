fcfb8f7cabd1ecb3a0d306dec213a21916cced68
VipulaNeranjana <vipulana17@gmail.com>
2023-02-24 01:01:08 +0530

    java fx io demo

---
+++ phase-1/java-se/file-io/io-baiscs/src/main/java/lk/ijse/dep10/io/Delete.java
@@ -0,0 +1,11 @@
+package lk.ijse.dep10.io;
+
+import java.io.File;
+
+public class Delete {
+    public static void main(String[] args) {
+        File file = new File("/home/vipula/Desktop/lala.txt");
+        file.delete();
+
+    }
+}
