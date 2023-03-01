160070988ebf0fc2fc7e09fca696c5b2d0756850
VipulaNeranjana <vipulana17@gmail.com>
2023-03-01 16:20:55 +0530

    reader and writer

---
+++ phase-1/java-se/file-io/io-baiscs/src/main/java/lk/ijse/dep10/io/ReaderDemo3.java
@@ -0,0 +1,32 @@
+package lk.ijse.dep10.io;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReaderDemo3 {
+    public static void main(String[] args) throws IOException {
+        File file = new File("something3.dep");
+        FileReader fr = new FileReader(file);
+//        char[] buffer = new char[0];
+//        int read = 0;
+//        String someText = "";
+//        while (true) {
+//            buffer = new char[100];
+//            read = fr.read(buffer);
+//
+//            if(read == -1) break;
+//            someText = new String(buffer,0,read);
+//
+//        }
+//        fr.close();
+//
+//        System.out.println(someText);
+
+        char[] newChar = new char[(int)file.length()];
+        int read = fr.read(newChar);
+        fr.close();
+
+        System.out.println(new String(newChar,0,read));
+    }
+}
